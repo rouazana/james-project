a974b65d6919f10f3182f2911285c9fd5c69aa77
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-08-27 17:53:15 +0200

    JAMES-1595 Mappings internal list is now immutable and not published anymore

--- server/data/data-api/src/main/java/org/apache/james/rrt/lib/Mappings.java
+++ server/data/data-api/src/main/java/org/apache/james/rrt/lib/Mappings.java
@@ -1,11 +1,8 @@
 package org.apache.james.rrt.lib;
 
-import java.util.Collection;
 
 public interface Mappings extends Iterable<String> {
 
-    Collection<String> getMappings();
-    
     boolean contains(String mapping);
 
     int size();
