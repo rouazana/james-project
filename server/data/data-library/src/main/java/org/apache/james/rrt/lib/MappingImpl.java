4b40893502b3463d8c868c56adf3b073043cbda5
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-08-31 16:58:39 +0200

    JAMES-1595 Mapping now have a getType method

--- server/data/data-library/src/main/java/org/apache/james/rrt/lib/MappingImpl.java
+++ server/data/data-library/src/main/java/org/apache/james/rrt/lib/MappingImpl.java
@@ -1,5 +1,7 @@
 package org.apache.james.rrt.lib;
 
+import org.apache.james.rrt.api.RecipientRewriteTable;
+
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 
@@ -13,6 +15,7 @@ public class MappingImpl implements Mapping {
     private final String mapping;
 
     public MappingImpl(String mapping) {
+        Preconditions.checkNotNull(mapping);
         this.mapping = mapping;
     }
     
@@ -30,6 +33,19 @@ public class MappingImpl implements Mapping {
     public Mapping appendDomain(String domain) {
         Preconditions.checkNotNull(domain);
         return new MappingImpl(mapping + "@" + domain);
+    }
+    
+    @Override
+    public Type getType() {
+        if (mapping.startsWith(RecipientRewriteTable.ALIASDOMAIN_PREFIX)) {
+            return Type.Domain;
+        } else if (mapping.startsWith(RecipientRewriteTable.REGEX_PREFIX)) {
+            return Type.Regex;
+        } else if (mapping.startsWith(RecipientRewriteTable.ERROR_PREFIX)) {
+            return Type.Error;
+        } else {
+            return Type.Address;
+        }
     }
     
     @Override
