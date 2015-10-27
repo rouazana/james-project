mbaechler/james

JAMES-1595 Promote Mappings to a actual object

container/cli/src/main/java/org/apache/james/cli/ServerCmd.java
@@ -18,6 +18,12 @@
  ****************************************************************/
 package org.apache.james.cli;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -28,13 +34,7 @@ import org.apache.commons.cli.PosixParser;
 import org.apache.james.cli.probe.ServerProbe;
 import org.apache.james.cli.probe.impl.JmxServerProbe;
 import org.apache.james.cli.type.CmdType;
-
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Map.Entry;
+import org.apache.james.rrt.lib.Mappings;
 
 /**
  * Command line utility for managing various aspect of the James server.
@@ -173,7 +173,7 @@ public class ServerCmd {
                 }
             } else if (CmdType.LISTUSERDOMAINMAPPINGS.equals(cmdType)) {
                 if (cmdType.hasCorrectArguments(arguments.length)) {
-                    Collection<String> userDomainMappings = probe.listUserDomainMappings(arguments[1], arguments[2]);
+                    Mappings userDomainMappings = probe.listUserDomainMappings(arguments[1], arguments[2]);
                     sCmd.print(userDomainMappings.toArray(new String[userDomainMappings.size()]), System.out);
                 } else {
                     printUsage();
@@ -253,11 +253,11 @@ public class ServerCmd {
         out.println();
     }
 
-    public void print(Map<String, Collection<String>> map, PrintStream out) {
+    public void print(Map<String, ? extends Iterable<String>> map, PrintStream out) {
         if (map == null)
             return;
 
-        for (Entry<String, Collection<String>> entry : map.entrySet()) {
+        for (Entry<String, ? extends Iterable<String>> entry : map.entrySet()) {
             out.print(entry.getKey());
             out.print("=");
             out.println(entry.getValue().toString());

data/data-hbase/src/main/java/org/apache/james/rrt/hbase/HBaseRecipientRewriteTable.java
@@ -19,8 +19,6 @@
 package org.apache.james.rrt.hbase;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,6 +35,8 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.james.rrt.api.RecipientRewriteTableException;
 import org.apache.james.rrt.hbase.def.HRecipientRewriteTable;
 import org.apache.james.rrt.lib.AbstractRecipientRewriteTable;
+import org.apache.james.rrt.lib.Mappings;
+import org.apache.james.rrt.lib.MappingsImpl;
 import org.apache.james.rrt.lib.RecipientRewriteTableUtil;
 import org.apache.james.system.hbase.TablePool;
 import org.slf4j.Logger;
@@ -60,7 +60,7 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
     protected void addMappingInternal(String user, String domain, String mapping) throws RecipientRewriteTableException {
         String fixedUser = getFixedUser(user);
         String fixedDomain = getFixedDomain(domain);
-        Collection<String> map = getUserDomainMappings(fixedUser, fixedDomain);
+        Mappings map = getUserDomainMappings(fixedUser, fixedDomain);
         if (map != null && map.size() != 0) {
             map.add(mapping);
             doUpdateMapping(fixedUser, fixedDomain, RecipientRewriteTableUtil.CollectionToMapping(map));
@@ -73,10 +73,10 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
      * @see org.apache.james.rrt.lib.AbstractRecipientRewriteTable#getUserDomainMappingsInternal(String, String)
      */
     @Override
-    protected Collection<String> getUserDomainMappingsInternal(String user, String domain) throws
+    protected Mappings getUserDomainMappingsInternal(String user, String domain) throws
             RecipientRewriteTableException {
         HTable table = null;
-        List<String> list = new ArrayList<String>();
+        Mappings list = MappingsImpl.empty();
         try {
             table = TablePool.getInstance().getRecipientRewriteTable();
             // Optimize this to only make one call.
@@ -96,14 +96,14 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
         return list;
     }
 
-    private void feedUserDomainMappingsList(HTable table, String user, String domain, Collection<String> list) throws
+    private void feedUserDomainMappingsList(HTable table, String user, String domain, Mappings list) throws
             IOException {
         Get get = new Get(Bytes.toBytes(getRowKey(user, domain)));
         Result result = table.get(get);
         List<KeyValue> keyValues = result.getColumn(HRecipientRewriteTable.COLUMN_FAMILY_NAME,
                                                     HRecipientRewriteTable.COLUMN.MAPPING);
         if (keyValues.size() > 0) {
-            list.addAll(RecipientRewriteTableUtil.mappingToCollection(Bytes.toString(keyValues.get(0).getValue())));
+            list.addAll(MappingsImpl.fromRawString(Bytes.toString(keyValues.get(0).getValue())));
         }
     }
 
@@ -111,10 +111,10 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
      * @see org.apache.james.rrt.lib.AbstractRecipientRewriteTable#getAllMappingsInternal()
      */
     @Override
-    protected Map<String, Collection<String>> getAllMappingsInternal() throws RecipientRewriteTableException {
+    protected Map<String, Mappings> getAllMappingsInternal() throws RecipientRewriteTableException {
         HTable table = null;
         ResultScanner resultScanner = null;
-        Map<String, Collection<String>> map = null;
+        Map<String, Mappings> map = null;
         try {
             table = TablePool.getInstance().getRecipientRewriteTable();
             Scan scan = new Scan();
@@ -128,11 +128,11 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
                     for (KeyValue keyValue : keyValues) {
                         String email = Bytes.toString(keyValue.getRow());
                         if (map == null) {
-                            map = new HashMap<String, Collection<String>>();
+                            map = new HashMap<String, Mappings>();
                         }
-                        Collection<String> list = map.get(email);
+                        Mappings list = map.get(email);
                         if (list == null) {
-                            list = new ArrayList<String>();
+                            list = MappingsImpl.empty();
                         }
                         list.add(Bytes.toString(keyValue.getRow()));
                         map.put(email, list);
@@ -207,7 +207,7 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
             RecipientRewriteTableException {
         String fixedUser = getFixedUser(user);
         String fixedDomain = getFixedDomain(domain);
-        Collection<String> map = getUserDomainMappings(fixedUser, fixedDomain);
+        Mappings map = getUserDomainMappings(fixedUser, fixedDomain);
         if (map != null && map.size() > 1) {
             map.remove(mapping);
             doUpdateMapping(fixedUser, fixedDomain, RecipientRewriteTableUtil.CollectionToMapping(map));

rouazana/james-project

server/container/cli/src/main/java/org/apache/james/cli/ServerCmd.java
@@ -18,8 +18,12 @@
  ****************************************************************/
 package org.apache.james.cli;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Joiner;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -40,12 +44,10 @@ import org.apache.james.cli.probe.impl.JmxServerProbe;
 import org.apache.james.cli.type.CmdType;
 import org.apache.james.cli.utils.ValueWithUnit;
 import org.apache.james.mailbox.model.Quota;
+import org.apache.james.rrt.lib.Mappings;
 
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Map.Entry;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
 
 /**
  * Command line utility for managing various aspect of the James server.
@@ -186,7 +188,7 @@ public class ServerCmd {
             print(probe.listMappings(), System.out);
             break;
         case LISTUSERDOMAINMAPPINGS:
-            Collection<String> userDomainMappings = probe.listUserDomainMappings(arguments[1], arguments[2]);
+            Mappings userDomainMappings = probe.listUserDomainMappings(arguments[1], arguments[2]);
             print(userDomainMappings.toArray(new String[0]), System.out);
             break;
         case ADDADDRESSMAPPING:
@@ -304,16 +306,16 @@ public class ServerCmd {
         return String.valueOf(value);
     }
 
-    private void print(Map<String, Collection<String>> map, PrintStream out) {
+    private void print(Map<String, ? extends Iterable<String>> map, PrintStream out) {
         if (map != null) {
-            for (Entry<String, Collection<String>> entry : map.entrySet()) {
+            for (Entry<String, ? extends Iterable<String>> entry : map.entrySet()) {
                 out.println(entry.getKey() + '=' + collectionToString(entry));
             }
             out.println();
         }
     }
 
-    private String collectionToString(Entry<String, Collection<String>> entry) {
+    private String collectionToString(Entry<String, ? extends Iterable<String>> entry) {
         return Joiner.on(',').join(entry.getValue());
     }
 

server/data/data-hbase/src/main/java/org/apache/james/rrt/hbase/HBaseRecipientRewriteTable.java
@@ -19,8 +19,6 @@
 package org.apache.james.rrt.hbase;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,6 +35,8 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.james.rrt.api.RecipientRewriteTableException;
 import org.apache.james.rrt.hbase.def.HRecipientRewriteTable;
 import org.apache.james.rrt.lib.AbstractRecipientRewriteTable;
+import org.apache.james.rrt.lib.Mappings;
+import org.apache.james.rrt.lib.MappingsImpl;
 import org.apache.james.rrt.lib.RecipientRewriteTableUtil;
 import org.apache.james.system.hbase.TablePool;
 import org.slf4j.Logger;
@@ -60,7 +60,7 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
     protected void addMappingInternal(String user, String domain, String mapping) throws RecipientRewriteTableException {
         String fixedUser = getFixedUser(user);
         String fixedDomain = getFixedDomain(domain);
-        Collection<String> map = getUserDomainMappings(fixedUser, fixedDomain);
+        Mappings map = getUserDomainMappings(fixedUser, fixedDomain);
         if (map != null && map.size() != 0) {
             map.add(mapping);
             doUpdateMapping(fixedUser, fixedDomain, RecipientRewriteTableUtil.CollectionToMapping(map));
@@ -73,10 +73,10 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
      * @see org.apache.james.rrt.lib.AbstractRecipientRewriteTable#getUserDomainMappingsInternal(String, String)
      */
     @Override
-    protected Collection<String> getUserDomainMappingsInternal(String user, String domain) throws
+    protected Mappings getUserDomainMappingsInternal(String user, String domain) throws
             RecipientRewriteTableException {
         HTableInterface table = null;
-        List<String> list = new ArrayList<String>();
+        Mappings list = MappingsImpl.empty();
         try {
             table = TablePool.getInstance().getRecipientRewriteTable();
             // Optimize this to only make one call.
@@ -96,14 +96,14 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
         return list;
     }
 
-    private void feedUserDomainMappingsList(HTableInterface table, String user, String domain, Collection<String> list) throws
+    private void feedUserDomainMappingsList(HTableInterface table, String user, String domain, Mappings list) throws
             IOException {
         Get get = new Get(Bytes.toBytes(getRowKey(user, domain)));
         Result result = table.get(get);
         List<KeyValue> keyValues = result.getColumn(HRecipientRewriteTable.COLUMN_FAMILY_NAME,
                                                     HRecipientRewriteTable.COLUMN.MAPPING);
         if (keyValues.size() > 0) {
-            list.addAll(RecipientRewriteTableUtil.mappingToCollection(Bytes.toString(keyValues.get(0).getValue())));
+            list.addAll(MappingsImpl.fromRawString(Bytes.toString(keyValues.get(0).getValue())));
         }
     }
 
@@ -111,10 +111,10 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
      * @see org.apache.james.rrt.lib.AbstractRecipientRewriteTable#getAllMappingsInternal()
      */
     @Override
-    protected Map<String, Collection<String>> getAllMappingsInternal() throws RecipientRewriteTableException {
+    protected Map<String, Mappings> getAllMappingsInternal() throws RecipientRewriteTableException {
         HTableInterface table = null;
         ResultScanner resultScanner = null;
-        Map<String, Collection<String>> map = null;
+        Map<String, Mappings> map = null;
         try {
             table = TablePool.getInstance().getRecipientRewriteTable();
             Scan scan = new Scan();
@@ -128,11 +128,11 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
                     for (KeyValue keyValue : keyValues) {
                         String email = Bytes.toString(keyValue.getRow());
                         if (map == null) {
-                            map = new HashMap<String, Collection<String>>();
+                            map = new HashMap<String, Mappings>();
                         }
-                        Collection<String> list = map.get(email);
+                        Mappings list = map.get(email);
                         if (list == null) {
-                            list = new ArrayList<String>();
+                            list = MappingsImpl.empty();
                         }
                         list.add(Bytes.toString(keyValue.getRow()));
                         map.put(email, list);
@@ -207,7 +207,7 @@ public class HBaseRecipientRewriteTable extends AbstractRecipientRewriteTable {
             RecipientRewriteTableException {
         String fixedUser = getFixedUser(user);
         String fixedDomain = getFixedDomain(domain);
-        Collection<String> map = getUserDomainMappings(fixedUser, fixedDomain);
+        Mappings map = getUserDomainMappings(fixedUser, fixedDomain);
         if (map != null && map.size() > 1) {
             map.remove(mapping);
             doUpdateMapping(fixedUser, fixedDomain, RecipientRewriteTableUtil.CollectionToMapping(map));