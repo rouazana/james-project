a37cd0748714ac2821294ac38dfeaee6be9b2be4
Antoine Duprat <antduprat@gmail.com>
2016-02-10 10:07:29 +0100

    JAMES-1688 Mailet context initialization

--- server/mailet/mailetcontainer-camel/src/main/java/org/apache/james/mailetcontainer/impl/JamesMailetContext.java
+++ server/mailet/mailetcontainer-camel/src/main/java/org/apache/james/mailetcontainer/impl/JamesMailetContext.java
@@ -42,6 +42,7 @@ import org.apache.mailet.base.RFC2822Headers;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -62,6 +63,7 @@ import java.util.Locale;
 import java.util.Vector;
 
 @SuppressWarnings("deprecation")
+@Singleton
 public class JamesMailetContext implements MailetContext, LogEnabled, Configurable {
 
     /**
