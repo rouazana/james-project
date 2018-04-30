ecc75e69a415ea6cfabed95eaba73e91be5306dc
Raphael Ouazana <raphael.ouazana@linagora.com>
2018-04-30 15:00:20 +0200

    Fix Eclise and checkstyle warnings and errors

--- server/protocols/protocols-smtp/src/main/java/org/apache/james/smtpserver/jmx/HookStats.java
+++ server/protocols/protocols-smtp/src/main/java/org/apache/james/smtpserver/jmx/HookStats.java
@@ -70,6 +70,8 @@ public class HookStats extends StandardMBean implements HookStatsMBean, Disposab
             case OK:
                 ok.incrementAndGet();
                 break;
+            case NONE:
+                throw new IllegalStateException();
         }
         all.incrementAndGet();
     }
