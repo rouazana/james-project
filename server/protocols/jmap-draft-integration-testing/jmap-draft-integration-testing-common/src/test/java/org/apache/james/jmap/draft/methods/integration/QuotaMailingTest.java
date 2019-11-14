80b9c6aa5cb5a260126b2f8c9078411255df95a3
Raphael Ouazana <raphael.ouazana@linagora.com>
2019-11-14 16:49:35 +0100

    fixup! JAMES-2949 Remove some implicit calls to Username::toString

--- server/protocols/jmap-draft-integration-testing/jmap-draft-integration-testing-common/src/test/java/org/apache/james/jmap/draft/methods/integration/QuotaMailingTest.java
+++ server/protocols/jmap-draft-integration-testing/jmap-draft-integration-testing-common/src/test/java/org/apache/james/jmap/draft/methods/integration/QuotaMailingTest.java
@@ -104,7 +104,7 @@ public abstract class QuotaMailingTest {
     @Test
     public void shouldSendANoticeWhenThresholdExceeded() throws Exception {
         jmapServer.getProbe(QuotaProbesImpl.class)
-            .setMaxStorage(MailboxConstants.USER_NAMESPACE + "&" + HOMER,
+            .setMaxStorage(MailboxConstants.USER_NAMESPACE + "&" + HOMER.asString(),
                 new SerializableQuotaValue<>(QuotaSize.size(100 * 1000)));
 
         bartSendMessageToHomer();
@@ -130,7 +130,7 @@ public abstract class QuotaMailingTest {
     @Test
     public void configurationShouldBeWellLoaded() throws Exception {
         jmapServer.getProbe(QuotaProbesImpl.class)
-            .setMaxStorage(MailboxConstants.USER_NAMESPACE + "&" + HOMER,
+            .setMaxStorage(MailboxConstants.USER_NAMESPACE + "&" + HOMER.asString(),
                 new SerializableQuotaValue<>(QuotaSize.size(100 * 1000)));
 
         bartSendMessageToHomer();
